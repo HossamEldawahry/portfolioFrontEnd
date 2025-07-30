e64ef23557bf77ba9966ca9387a9b2d3c72e3579
Hossam Mostafa <hossamarab2024@gmail.com>
2025-07-30 09:34:54 +0300

    Update Api URl

--- src/Core/server/baseUrl.ts
+++ src/Core/server/baseUrl.ts
@@ -1,5 +1,5 @@
-export const imageBaseUrl = 'https://localhost:7003';
-export const profileBaseUrl = 'https://localhost:7003/api/Profile';
-export const projectBaseUrl = 'https://localhost:7003/api/Projects';
-export const skillBaseUrl = 'https://localhost:7003/api/Skills';
-export const messageBaseUrl = 'https://localhost:7003/api/Message';
+export const imageBaseUrl = 'http://hossamportfolioapi.runasp.net';
+export const profileBaseUrl = 'http://hossamportfolioapi.runasp.net/api/Profile';
+export const projectBaseUrl = 'http://hossamportfolioapi.runasp.net/api/Projects';
+export const skillBaseUrl = 'http://hossamportfolioapi.runasp.net/api/Skills';
+export const messageBaseUrl = 'http://hossamportfolioapi.runasp.net/api/Message';
