c3a733fd4ab6c41fcfcbc747b52617b61a774e21
Hossam Mostafa <hossamarab2024@gmail.com>
2025-07-31 11:22:07 +0300

    Add Loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,21 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from '../Shared/Components/navigation/navigation.component';
 import { FooterComponent } from '../Shared/Components/footer/footer.component';
+import { LoadingService } from '../Core/Services/loading/loading.service';
+import { Observable } from 'rxjs';
+import { LoadingComponent } from '../Shared/Components/loading/loading.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavigationComponent,FooterComponent],
+  imports: [RouterOutlet, NavigationComponent,FooterComponent,LoadingComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'portfolio-FrontEnd';
+  title = 'Hossam Mostafa | Portfolio';
+  loading$ : Observable<boolean>;
+  constructor(private loadingService: LoadingService) {
+    this.loading$ = this.loadingService.loading$;
+  }
 }
